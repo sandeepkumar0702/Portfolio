4251f9fa4dd7576e62219db3fbeef4acdf435486
Sandeep Kumar <sandeepkumar073113@gmail.com>
2025-04-13 21:45:51 +0530

    [Sandeep] ADD:Adding the Proect page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContactForm from "./Components/ContactForm";
 import Home from "./Components/Home";
+import Projects from "./Components/Project";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<ContactForm />} />
+        <Route path="/project" element={<Projects />} />
       </Routes>
     </Router>
   );
